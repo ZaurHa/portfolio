fafab5cee46ca18635494368e5baba70d8e3d79b
Zaurde <zaurhh88@gmail.com>
2025-07-05 14:14:12 +0200

    Fix: Add default export to leistungen/page.tsx

---
+++ app/leistungen/page.tsx
@@ -0,0 +1,9 @@
+export default function Leistungen() {
+  return (
+    <div>
+      {/* Hier dein Inhalt */}
+      <h1>Leistungen</h1>
+      <p>Hier stehen deine Leistungen...</p>
+    </div>
+  );
+}
